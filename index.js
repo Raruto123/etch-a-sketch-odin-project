3841ea9b224bf0d69750dd8c734d542918ab670c
mohamedsane <sanem9206@gmail.com>
2024-09-02 01:00:38 +0000

    Start the project
    I draw the first cube dynamically with JavaScript

---
+++ index.js
@@ -0,0 +1,10 @@
+
+let square = document.createElement("div");
+const container = document.querySelector(".container");
+container.append(square);
+square.classList.add("square");
+
+
+function create16x16Grid () {
+
+}
