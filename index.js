31219f847e49cc54a7048451c03ab180376f40e8
mohamedsane <sanem9206@gmail.com>
2024-09-12 00:54:36 +0000

    Update the app
    The logic to use different color to draw was reworked, currently on an eraser feature

--- index.js
+++ index.js
@@ -13,19 +13,46 @@ nouvelle grille de la dimension de son input doit être généré
 
 let squares16x16 = new Array();
 const container = document.querySelector(".container");
-let userRangeChoice = document.getElementById("user-range-choice");
-let userColorChoice = document.getElementById("user-color-choice");
-let numberOfSquaresText = document.querySelector(".number-of-squares");
+const userRangeChoice = document.getElementById("user-range-choice");
+const userColorChoice = document.getElementById("user-color-choice");
+const numberOfSquaresText = document.querySelector(".number-of-squares");
+const userEraser = document.getElementById("user-eraser");
 let isDrawing = false;
+
+
+function eraseSquares() {
+    container.innerHTML = "";
+};
+
+userEraser.addEventListener("input", () => {
+    eraseSquares();
+});
 
 numberOfSquaresText.textContent = userRangeChoice.value;
 
+function drawing(squares) {
+    squares.addEventListener("click", (event) => {
+        event.target.style.setProperty("background-color", userColorChoice.value)
+    });
 
+    squares.addEventListener("mousedown", () => {
+        isDrawing = true;
+    });
+    squares.addEventListener("mousemove", (event) => {
+        if (isDrawing) {
+            event.target.style.setProperty("background-color", userColorChoice.value)
+            console.log("click pressed");
+        }
+    })
+    squares.addEventListener("mouseup", () => {
+        isDrawing = false
+    })
+}
 
 function create16x16Grid (grid) {
 
     container.innerHTML = ""; // Initialize the container every time
-    // userColorChoice.value = "#000000"
+    userColorChoice.value = "#000000";//Starting color 
 
 
     for (let i = 0; i < grid; i++) {
@@ -33,6 +60,7 @@ function create16x16Grid (grid) {
         container.append(squares16x16[i]);//Append each element
         squares16x16[i].classList.add("square16x16");//Add a style class for the new elements
 
+        //Hover effect on squares
         squares16x16[i].addEventListener("mouseover", (event) => {
             event.target.classList.add("square16x16Hover");
             console.log("mouseover");
@@ -40,37 +68,9 @@ function create16x16Grid (grid) {
         squares16x16[i].addEventListener("mouseleave", (event) => {
             event.target.classList.remove("square16x16Hover");
         });
-        squares16x16[i].addEventListener("click", (event) => {
-            event.target.classList.add("square16x16Click");
-            console.log("click black")
-        })
-        squares16x16[i].addEventListener("mousedown", () => {
-            isDrawing = true;
-        });
-        squares16x16[i].addEventListener("mousemove", (event) => {
-            if (isDrawing) {
-                event.target.classList.add("square16x16Click");
-                console.log("click pressed");
-            }
-        })
-        squares16x16[i].addEventListener("mouseup", (event) => {
-            isDrawing = false
-        })
 
-
-        userColorChoice.addEventListener("input", (colorEvent) => {
-            squares16x16[i].addEventListener("click", (squaresEvent) => {
-                console.log("click coloured")
-            squaresEvent.target.style.setProperty("background-color", colorEvent.target.value);
-            })
-        });
-        // squares16x16[i].addEventListener("click", (event) => {
-        //     console.log("click");
-        //     squares16x16[i].addEventListener("mousedown", (event) => {
-        //         console.log("mouse enter")
-        //         event.target.classList.add("square16x16Click");
-        //     })
-        // });
+        //Drawing effects
+        drawing(squares16x16[i]);
 
     }
 
@@ -79,25 +79,12 @@ function create16x16Grid (grid) {
 
 create16x16Grid(32);
 
-// function createGrid(numberOfSquares) {
 
-//     container.innerHTML = ""; // Réinitialiser le conteneur
-
-//         for (let i = 0; i < numberOfSquares; i++) {
-//         let square = document.createElement("div");
-//         // square.classList.add("square16x16");
-//         // square.style.width = `${squareSize}%`;
-//         // square.style.height = `${squareSize}vw`; // Maintenir un ratio carré
-//         container.appendChild(square);
-//         square.classList.add("square16x16");
-//     }
-// }
 
 
 
 
 userRangeChoice.addEventListener("input", (event) => {
     numberOfSquaresText.textContent = event.target.value;
-    // createGrid(parseInt(numberOfSquaresText.textContent))
     create16x16Grid(parseInt(numberOfSquaresText.textContent));
 });
